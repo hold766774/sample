63d12b9a81faa5b342ca51c8e3c1ceab8d55ee8c
hold766774 <1106246044@qq.com>
2017-09-02 16:51:18 +0000

    user curd auth

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,6 +8,13 @@ use Illuminate\Support\Facades\Auth;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth',[
+            'except'=>['show','create','store']
+        ]);
+    }
+
     public function create()
     {
         return view('users.create');
@@ -31,6 +38,29 @@ class UsersController extends Controller
         ]);
         Auth::login($user);
         session()->flash('success','欢迎，您将在这里开启一段新的路途');
-        return redirect()->route('users.show',[$user]);
+        return redirect()->intended(route('users.show',[$user]));
+    }
+    public function edit(User $user)
+    {
+        $this->authorize('update',$user);
+        return view('users.edit',compact('user'));
+    }
+    public function update(User $user,Request $request)
+    {
+        $this->validate($request,[
+            'name'=>'required|max:50',
+            'password'=>'nullable|confirmed|min:6',
+        ]);
+        $this->authorize('update',$user);
+
+        $data=[];
+        $data['name']=$request->name;
+        if($request->password)
+        {
+            $data['password']=bcrypt($request->pqssword);
+        }
+        $user->update($data);
+        session()->flash('success','个人资料更新成功');
+        return redirect()->route('users.show',$user->id);
     }
 }
--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -16,7 +16,7 @@ use \Illuminate\Support\Facades\Auth;
                             </a>
                             <ul class="dropdown-menu">
                                 <li><a href="{{ route('users.show', Auth::user()->id) }}">个人中心</a>
-                                <li><a href="#">编辑资料</a>
+                                <li><a href="{{ route('users.edit', Auth::user()->id) }}">编辑资料</a>
                                 <li class="divider">
                                 <li>
                                     <a id="logout" href="#">
